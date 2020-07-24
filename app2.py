e7acac5edeb40b33e9e670660a7be2bf6581819e
davidtracy6984 <43183422+davidtracy6984@users.noreply.github.com>
2020-07-23 21:29:07 -0700

    MoreStuff

--- app2.py
+++ app2.py
@@ -18,8 +18,8 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 Consumption = Base.classes.consumption
-Generation = Base.classes.generation
-Emissions = Base.classes.emissions
+#Generation = Base.classes.generation
+#Emissions = Base.classes.emissions
 
 app2 = Flask(__name__)
 
@@ -27,36 +27,40 @@ app2 = Flask(__name__)
 def consumption():
     session = Session(engine)
 
-    con_query = session.query(Consumption.Year,Consumption.State,Consumption.TypeOfProducer,Consumption.EnergySource,Consumption.UseOfElectricity).all()
-    #con_query = session.query(Consumption).all()
+    #con_query = session.query(Consumption.Year,Consumption.State,Consumption.TypeOfProducer,Consumption.EnergySource,Consumption.UseOfElectricity).all()
+    con_query = session.query(Consumption.Year)#.all()
     session.close()
 
     con_lst = []
-    for result in con_query:
-        con_lst.append({"Year":result.Year,
-        "State":result.State,
-        "Producer":result.TypeOfProducer,
-        "Source":result.EnergySource,
-        "Consumption":result.UseOfElectricity})
+    for year in con_query:
+        dictionary = {}
+        dictionary["Year"]=year
+        con_lst.append(dictionary)
+    #     con_lst.append({"Year":result.Year#,
+    #     # "State":result.State,
+    #     # "Producer":result.TypeOfProducer,
+    #     # "Source":result.EnergySource,
+    #     # "Consumption":result.UseOfElectricity
+    #     })
 
     return jsonify(con_lst)
 
-def generation():
-    session = Session(engine)
+# def generation():
+#     session = Session(engine)
 
-    gen_query = session.query(Generation.Year,Generation.State,Generation.TypeOfProducer,Generation.EnergySource,Generation.Generated).all()
-    #con_query = session.query(Consumption).all()
-    session.close()
+#     gen_query = session.query(Generation.Year,Generation.State,Generation.TypeOfProducer,Generation.EnergySource,Generation.Generated).all()
+#     #con_query = session.query(Consumption).all()
+#     session.close()
 
-    gen_lst = []
-    for result in gen_query:
-        gen_lst.append({"Year":result.Year,
-        "State":result.State,
-        "Producer":result.TypeOfProducer,
-        "Source":result.EnergySource,
-        "Generated":result.Generated})
+#     gen_lst = []
+#     for result in gen_query:
+#         gen_lst.append({"Year":result.Year,
+#         "State":result.State,
+#         "Producer":result.TypeOfProducer,
+#         "Source":result.EnergySource,
+#         "Generated":result.Generated})
 
-    return jsonify(gen_lst)
+#     return jsonify(gen_lst)
 
 
 if __name__ == "__main__":
