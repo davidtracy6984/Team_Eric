82d11d608201f45f5e5104e93926f1ccc6ad602d
davidtracy6984 <43183422+davidtracy6984@users.noreply.github.com>
2020-07-24 17:20:59 -0700

    App Edit

--- app2.py
+++ app2.py
@@ -55,36 +55,8 @@ def consumption():
 
 
     return jsonify(con_lst)
-
+# 
 def generation():
-<<<<<<< HEAD
-=======
-    session = Session(engine)
-
-    gen_query = session.query(
-        Generation.Year,
-        Generation.State,
-        Generation.TypeOfProducer,
-        Generation.EnergySource,
-        Generation.Generated)
-    
-    session.close()
-
-    gen_lst = []
-    for year,state,typeofproducer,energysource,generated in gen_query:
-        gen_dict = {}
-        gen_dict["Year"] = year
-        gen_dict["State"] = state
-        gen_dict["TypeOfProducer"] = typeofproducer
-        gen_dict["EnergySource"] = energysource
-        gen_dict["Generated"] = int(generated)
-        gen_lst.append(gen_dict)
-
-    return jsonify(gen_lst)
-
-
-def emissions():
->>>>>>> addf4cf9ffe62ac84c90386f0df82c488ffbed2b
     session = Session(engine)
 
     gen_query = session.query(
